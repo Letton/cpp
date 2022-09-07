3ba17951d3d27826390a1d4748a3c27190c55870
Letton <admin@letton.tech>
2022-09-07 09:34:44 +0300

    Solution 4

--- Source.cpp
+++ Source.cpp
@@ -4,17 +4,12 @@
 using namespace std;
 
 int main() {
-	float a, b, c;
-	cin >> a >> b >> c;
-	float d = b * b - 4 * a * c;
-	if (d > 0) {
-		cout << (-b + sqrt(d)) / (2 * a) << '\n';
-		cout << (-b - sqrt(d)) / (2 * a);
+	bool time, curtains, lamp;
+	cin >> time >> curtains >> lamp;
+	if ((time == 1 and curtains == 1) or (lamp == 1)) {
+		cout << "Light";
 	}
-	if (d == 0) {
-		cout <<  -b / (2 * a);
-	}
-	if (d < 0) {
-		cout << "No solution";
+	else {
+		cout << "Dark";
 	}
 }
