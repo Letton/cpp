ea046a2ac1535b8c152e7be7bd1d855d7ab014c6
Letton <admin@letton.tech>
2022-09-07 10:25:17 +0300

    Solution 10

--- Source.cpp
+++ Source.cpp
@@ -5,13 +5,9 @@
 
 using namespace std;
 
-float func(float x) {
-	return ((x * x - 2 * x + 2) / (x - 1));
-}
-
-
 int main() {
-	for (float i = -4; i <= 4; i += 0.5) {
-		cout << "x = " << i << "; y = " << func(i) << '\n';
-	}
+	float S, p, n;
+	cin >> S >> p >> n;
+	float r = p / 100;
+	cout << "m = " << S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1));
 }
