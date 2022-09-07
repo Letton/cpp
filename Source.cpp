442fb2b854f8346f7e8b137cefa9aae639fa6976
Letton <admin@letton.tech>
2022-09-07 09:30:26 +0300

    Solution 3

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-
 int main() {
-	float b, c;
-	cin >> b >> c;
-	cout << -c / b;
+	float a, b, c;
+	cin >> a >> b >> c;
+	float d = b * b - 4 * a * c;
+	if (d > 0) {
+		cout << (-b + sqrt(d)) / (2 * a) << '\n';
+		cout << (-b - sqrt(d)) / (2 * a);
+	}
+	if (d == 0) {
+		cout <<  -b / (2 * a);
+	}
+	if (d < 0) {
+		cout << "No solution";
+	}
 }
