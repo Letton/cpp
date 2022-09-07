68dbc51e64b88794a6480ae06b9b045ff233cf89
Letton <admin@letton.tech>
2022-09-07 09:44:22 +0300

    Solution 5

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,13 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-	bool time, curtains, lamp;
-	cin >> time >> curtains >> lamp;
-	if ((time == 1 and curtains == 1) or (lamp == 1)) {
-		cout << "Light";
-	}
-	else {
-		cout << "Dark";
-	}
+	float R, r, h, l;
+	cin >> R >> r >> h >> l;
+	cout << "V = " << 1.0 / 3.0 * M_PI * h * (R * R + R * r + r * r) << '\n';
+	cout << "S = " << M_PI * (R * R + (R + r) * l + r * r);
 }
