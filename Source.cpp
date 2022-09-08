2d3e6fece15e39b23f8556d13906546cedac714a
Letton <admin@letton.tech>
2022-09-08 10:12:49 +0300

    Solution 12

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,26 @@
 #define _USE_MATH_DEFINES
 
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <cmath>
 
 using namespace std;
 
 int main() {
-	float S, m, n;
-	cin >> S >> m >> n;
-	float p = 0.00001;
-	float r = p / 100;
-	while (m > (S * r * pow((1 + r), n) / (12 * (pow((1 + r), n) - 1)))) {
-		p+=0.00001;
-		r = p / 100;
+	cout << "Input filename\n";
+	string filename, fileline;
+	cin >> filename;
+	ifstream file("./" + filename);
+	if (!file) {
+		cout << "Error opening file\n";
+		return -1;
 	}
-	cout << floor(p);
+	while (getline(file, fileline))
+	{
+		cout << fileline << "\n";
+	}
+
+	file.close();
+
 }
