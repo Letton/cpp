b58b26ede3c15d0c2bd61201a80349b8e9dfbb04
Letton <admin@letton.tech>
2022-09-14 10:06:50 +0300

    Solution 16

--- Source.cpp
+++ Source.cpp
@@ -8,28 +8,12 @@
 
 using namespace std;
 
-int main() {
-	cout << "Input filename\n";
-	string filename, fileline;
-	cin >> filename;
-	ofstream fileout(filename);
-	double arr[10];
-	for (int i = 0; i < 10; ++i) {
-		cin >> arr[i];
-	}
-	for (int i = 0; i < 10; ++i) {
-		fileout << arr[i] << " ";
-	}
-	fileout.close();
-	ifstream filein(filename);
-	for (int i = 0; i < 10; ++i) {
-		filein >> arr[i];
-	}
-	filein.close();
-	double sum = 0;
-	for (int i = 0; i < 10; ++i) {
-		sum += arr[i];
-	}
-	cout << sum;
+int sign(int x) {
+	return x > 0 ? 1 : x == 0 ? 0 : -1;
+}
 
+int main() {
+	int n;
+	cin >> n;
+	cout << sign(n);
 }
