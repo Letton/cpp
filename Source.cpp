f634b7f3ddcbc87c4b4b1ca4333f9bebc7938adc
Letton <admin@letton.tech>
2022-09-21 17:23:56 +0300

    Solution 18

--- Source.cpp
+++ Source.cpp
@@ -8,44 +8,13 @@
 
 using namespace std;
 
-double rectangle_square(double a, double b) {
-	return a * b;
-}
-
-double circle_square(double r) {
-	return r * r * 3.14;
-}
-
-
 int main() {
-	char c;
-	cout << "Input R for retangle, C for circle\n";
-	cin >> c;
-	if (c == 'R') {
-		double a, b;
-		cout << "Input a, b\n";
-		cin >> a >> b;
-		if ((a < 0) || (b < 0)) {
-			cout << "Incorrect input";
-			return -1;
-		}
-		cout << "S = ab\n";
-		cout << "S = " << a << " * " << b << " = " << rectangle_square(a, b);
+	cout << "_________________________________________________\n";
+	for (int i = 0; i < 6; ++i) {
+		cout << "********_________________________________________\n";
 	}
-	else if (c == 'C') {
-		double r;
-		cout << "Input radius\n";
-		cin >> r;
-		if (r < 0) {
-			cout << "Incorrect input";
-			return -1;
-		}
-		cout << "S = pi * r^2\n";
-		cout << "S = 3,14 * " << r << "^2 = " << circle_square(r);
-	}
-	else {
-		cout << "Incorrect input";
-		return -1;
+	for (int i = 0; i < 6; ++i) {
+		cout << "_________________________________________________\n";
 	}
 	return 0;
 }
