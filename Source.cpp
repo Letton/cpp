77291dd38e304a72773bffcae7684a75b9b7e3b3
Letton <admin@letton.tech>
2022-09-28 16:42:22 +0300

    Solution 19

--- Source.cpp
+++ Source.cpp
@@ -9,32 +9,20 @@
 
 using namespace std;
 
-int binarySearch(int arr[], int p, int r, int num) {
-	if (p <= r) {
-		int mid = (p + r) / 2;
-		if (arr[mid] == num)
-			return mid;
-		if (arr[mid] > num)
-			return binarySearch(arr, p, mid - 1, num);
-		if (arr[mid] < num)
-			return binarySearch(arr, mid + 1, r, num);
-	}
-	return -1;
-}
 
 int main() {
 	int w, h;
 	w = 80;
 	h = 20;
 	cin >> w >> h;
-	vector <vector<int>> data(h);
+	vector <vector<int>> data(h + 1);
 	vector <int> test;
 	int counter = 0;
 	for (double i = 0; i < (0.2 * w); i += 0.2) {
-		data[floor(h / 2 - (sin(i) * h / 2))].push_back(counter);
+		data[round(h / 2 - (sin(i) * h / 2))].push_back(counter);
 		++counter;
 	}
-	for (int i = 0; i < h; ++i) {
+	for (int i = 0; i < (h + 1); ++i) {
 		for (int j = 0; j < data[i].size(); ++j) {
 			if (j == 0) {
 				for (int k = 0; k < data[i][j]; ++k) {
