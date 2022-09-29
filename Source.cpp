b7b305e287f47ed82fbcc3a3db69a2945494a07d
Letton <admin@letton.tech>
2022-09-29 10:26:58 +0300

    Solution 19

--- Source.cpp
+++ Source.cpp
@@ -14,30 +14,105 @@ int main() {
 	int w, h;
 	w = 80;
 	h = 20;
-	cin >> w >> h;
 	vector <vector<int>> data(h + 1);
-	vector <int> test;
 	int counter = 0;
 	for (double i = 0; i < (0.2 * w); i += 0.2) {
 		data[round(h / 2 - (sin(i) * h / 2))].push_back(counter);
 		++counter;
 	}
-	for (int i = 0; i < (h + 1); ++i) {
+	for (int i = 0; i < h / 2; ++i) {
 		for (int j = 0; j < data[i].size(); ++j) {
 			if (j == 0) {
 				for (int k = 0; k < data[i][j]; ++k) {
-					cout << " ";
+					if (k == (w / 2 - 1))
+						cout << '|';
+					else
+						cout << " ";
 				}
-				cout << "-";
+				if (data[i][j] == (w / 2 - 1))
+					cout << '|';
+				else
+					cout << "*";
 			}
 			else {
 				for (int k = data[i][j - 1] + 1; k < data[i][j]; ++k) {
-					cout << " ";
+					if (k == (w / 2 - 1))
+						cout << '|';
+					else
+						cout << " ";
 				}
-				cout << "-";
+				if (data[i][j] == (w / 2 - 1))
+					cout << '|';
+				else
+					cout << "*";
 			}
+		}
+		for (int j = data[i][data[i].size() - 1]; j <= w; ++j) {
+			if (j == (w / 2 - 2))
+				cout << "|";
+			else
+				cout << " ";
+		}
+		cout << '\n';
+	}
+	int currentLine = h / 2;
+	for (int j = 0; j < data[currentLine].size(); ++j) {
+		if (j == 0) {
+			for (int k = 0; k < data[h / 2][j]; ++k) {
+				if (k == w / 2) 
+					cout << "+";
+				else
+					cout << "-";
+
+			}
+			cout << "-";
+		}
+		else {
+			for (int k = data[currentLine][j - 1] + 1; k < data[currentLine][j]; ++k) {
+				if (k == (w / 2 - 1))
+					cout << "+";
+				else
+					cout << "-";
+			}
+			cout << "-";
+		}
+	}
+	for (int j = data[currentLine][data[currentLine].size() - 1]; j <= (w - 1); ++j) {
+		cout << "-";
+	}
+	cout << ">";
+	cout << "\n";
+	for (int i = h / 2 + 1; i < h + 1; ++i) {
+		for (int j = 0; j < data[i].size(); ++j) {
+			if (j == 0) {
+				for (int k = 0; k < data[i][j]; ++k) {
+
+					if (k == (w / 2 - 1))
+						cout << '|';
+					else
+						cout << " ";
+
+				}
+				cout << "*";
+			}
+			else {
+				for (int k = data[i][j - 1] + 1; k < data[i][j]; ++k) {
+					if (k == (w / 2 - 1))
+						cout << '|';
+					else
+						cout << " ";
+				}
+				cout << "*";
+			}
+		}
+		for (int j = data[i][data[i].size() - 1]; j <= w; ++j) {
+			if (j == (w / 2 - 2))
+				cout << "|";
+			else
+				cout << " ";
 		}
 		cout << '\n';
 	}
 	return 0;
 }
+
