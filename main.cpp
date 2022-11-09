7146e625a709093577272dd3873766568fd0adf8
Letton <admin@letton.tech>
2022-11-09 20:40:12 +0300

    5.5

--- main.cpp
+++ main.cpp
@@ -2,52 +2,25 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
+#include <map>
 
 using namespace std;
 
-struct STRING_WITH_WORDS {
-    string str;
-    int words = 0;
-};
-
-bool comp(STRING_WITH_WORDS i, STRING_WITH_WORDS j) {
-    return (i.words < j.words);
-}
-
-unsigned countWords(const char *str) {
-    int state = 0;
-    unsigned wc = 0;
-    while (*str)
-    {
-        if (*str == ' ' || *str == '\n' || *str == '\t')
-            state = 0;
-        else if (state == 0)
-        {
-            state = 1;
-            ++wc;
-        }
-        ++str;
-    }
-    return wc;
-}
-
 int main() {
-    vector <STRING_WITH_WORDS> arr;
-    cout << "Input number of strings\n";
+    map <int, int> numbers;
+    string filename, text;
+    cout << "Input filename\n";
+    cin >> filename;
+    ifstream filein(filename);
     int n;
-    cin >> n;
-    cout << "Input strings\n";
-    cin.ignore();
-    for (int i = 0; i < n; ++i) {
-        STRING_WITH_WORDS sw;
-        getline(cin, sw.str);
-        const char* str = sw.str.c_str();
-        sw.words = countWords(str);
-        arr.push_back(sw);
+    while (filein >> n) {
+        numbers[n] += 1;
     }
-    sort(arr.begin(), arr.end(), comp);
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i].str << " : " << arr[i].words << " words" << "\n";
+    int max_n, maxx = 0;
+    for (auto it = numbers.begin(); it != numbers.end(); ++it)
+    {
+        cout << (*it).first << " : " << (*it).second << endl;
     }
     return 0;
 }
