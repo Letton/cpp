c0e5407eeb84e4cf7d15b61d00c3375a804a796b
Letton <admin@letton.tech>
2022-11-30 17:38:59 +0300

    5.7

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 
 template <typename T>
+double bine(T n) {
+    double index = pow(5, 0.5);
+
+    double left = (1 + index) / 2;
+    double right = (1 - index) / 2;
+
+    return round((pow(left, n) - pow(right, n)) / index);
+}
+
+
+template <typename T>
 T fibonacci(T n) {
     if (n == 1 or n == 2)
         return 1;
@@ -19,6 +30,7 @@ int main() {
         cout << "Incorrect input";
         return -1;
     }
+    cout << "Fibonacci number by bine is " << bine <long long> (n) << "\n";
     cout << "Fibonacci number is " << fibonacci <long long> (n);
     return 0;
 }
