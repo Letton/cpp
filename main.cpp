73ce0ec200293d2d79335951d371e3bac0a0b740
Letton <admin@letton.tech>
2022-12-13 23:46:12 +0300

    Bulls and cows

--- main.cpp
+++ main.cpp
@@ -1,30 +1,72 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 using namespace std;
 
-int moves = 0;
-
-void hanoi(int n, int i, int k) {
-    moves += 1;
-    if (n == 1) {
-        cout << "Move disk 1 from " << i << " to " << k << '\n';
-    } else {
-        int temp = 6 - i - k;
-        hanoi(n - 1, i, temp);
-        cout << "Move disk " << n << " from " <<  i << " to " << k << '\n';
-        hanoi(n - 1, temp, k);
+string generateRandomNumber() {
+    string str;
+    while (str.length() != 4) {
+        char randDigit = char('0' + rand() % 10);
+        if (!str.contains(randDigit)) {
+            str += randDigit;
+        }
     }
+    return str;
+}
+
+bool isCorrectNumber(string & str) {
+    if (str.length() != 4)
+        return false;
+    for (auto & chr : str) {
+        if (count(str.begin(), str.end(), chr) > 1) {
+            return false;
+        }
+        if (std::isdigit(chr) == 0)
+            return false;
+    }
+    return true;
 }
 
 
 
 int main() {
-    int n;
-    cout << "Input count of disks\n";
-    cin >> n;
-    hanoi(n, 1, 2);
-    cout << "Total transfers: " << moves;
+    srand(time(nullptr));
+    cout << "This program \"conceives\" a four-digit number that does not contain two of the same digits!\n"
+            "You enter your number and the computer reports the number of pluses (correctly guessed\n"
+            "numbers, i.e., standing in their places) and minuses (numbers that are in the intended number,\n"
+            "but elsewhere).\n";
+    string randomNumber = generateRandomNumber();
+    string str;
+    int pluses = 0, minuses = 0;
+    while (pluses != 4) {
+        pluses = 0, minuses = 0;
+        cout << "Input number\n";
+        getline(cin, str);
+        while (!isCorrectNumber(str)) {
+            cout << "Please, input correct number\n";
+            getline(cin, str);
+        }
+        for (int i = 0; i < str.length(); ++i) {
+            if (randomNumber.contains(str[i])) {
+                if (randomNumber.find(str[i]) == i) {
+                    pluses += 1;
+                }
+                if (randomNumber.find(str[i]) != i) {
+                    minuses += 1;
+                }
+            }
+        }
+        cout << randomNumber << "\n";
+        for (int i = 0; i < pluses; ++i) {
+            cout << "+";
+        }
+        for (int i = 0; i < minuses; ++i) {
+            cout << "-";
+        }
+        cout << "\n";
+    }
+    cout << "You WON!";
     return 0;
 }
